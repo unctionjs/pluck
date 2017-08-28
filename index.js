0d5141a89f0d09f4188a37282d54b352f7c025dc
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-27 19:46:29 -0700

    We have a new, more formal, name for iterables

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import keyChain from "@unction/keychain"
 import mapValues from "@unction/mapvalues"
 
 export default function pluck (keychain: KeyChainType): Function {
-  return function pluckKeychain (iterable: IterableType): IterableType {
-    return mapValues(keyChain(keychain))(iterable)
+  return function pluckKeychain (functor: FunctorType): FunctorType {
+    return mapValues(keyChain(keychain))(functor)
   }
 }
