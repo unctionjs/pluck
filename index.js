63ec9dafa962b28ff7667ca49ec0e8352b3c3ace
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-27 13:02:26 -0700

    Removing packages we don't need

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-import dig from "@unction/dig"
-import mapValues from "@unction/mapvalues"
-
-import type {KeyChainType} from "types"
-import type {UnaryFunctionType} from "types"
-import type {KeyedEnumerableType} from "types"
-
-export default function pluck (keychain: KeyChainType): UnaryFunctionType {
-  return function pluckKeychain (functor: KeyedEnumerableType): KeyedEnumerableType {
-    return mapValues(dig(keychain))(functor)
-  }
+import dig from "@unction/dig";
+import mapValues from "@unction/mapvalues";
+export default function pluck(keychain) {
+  return function pluckKeychain(functor) {
+    return mapValues(dig(keychain))(functor);
+  };
 }
